3a51fb0ffd983c6fca5307f3c6946e076a7a893d
palak273 <38990907+palak273@users.noreply.github.com>
2020-07-30 03:11:06 +0530

    first commit

---
+++ src/main/java/com/java/docker/form/FormController.java
@@ -0,0 +1,23 @@
+package com.java.docker.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormController {
+
+    @GetMapping("/product")
+    public String productForm(Model model){
+        model.addAttribute("product",new Product());
+        return "product";
+    }
+
+    @PostMapping("/product")
+    public String productSubmit(@ModelAttribute Product product) {
+        return "product";
+    }
+}
+
---
+++ src/main/java/com/java/docker/form/Product.java
@@ -0,0 +1,42 @@
+package com.java.docker.form;
+
+public class Product {
+
+    private long id;
+    private String name;
+    private float price;
+    private int quantity;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
+
